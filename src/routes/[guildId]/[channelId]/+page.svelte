4238f3e427b22b26d5ddd8946be37bb26583f6f2
jami <benjamin6903@riseup.net>
2026-05-02 22:38:59 +0100

    add channel content area

---
+++ src/routes/[guildId]/[channelId]/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<div class="channel-layout">
+    <div class="title-bar"></div>
+    <div class="content-area"></div>
+</div>
+
+<style>
+    .channel-layout {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+    }
+
+    .title-bar {
+        height: 50px;
+        width: 100%;
+    }
+
+    .content-area {
+        border: 1px solid rgb(255 255 255 / 0.2);
+        border-radius: 25px 25px 0px 0px;
+        width: 100%;
+        flex-grow: 1;
+    }
+</style>
